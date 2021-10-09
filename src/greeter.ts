8ff923052317c9a31a6f8ebbbdfc0bca0b9aeaad
Szabó-Battancs Benedek <h880561@stud.u-szeged.hu>
2021-10-09 23:21:01 +0200

    functions for sorting lower and uppercase names

--- src/greeter.ts
+++ src/greeter.ts
@@ -6,27 +6,99 @@ export class Greeter{
         this.name = null
     }
 
-    public greeting(...name :string[]): String{
-        if(name.length === 0){
-            return "Greetings, my friend!"
+
+    private isItUpperCase(input: string): boolean{
+        return input === input.toUpperCase();
+    }
+
+    //constracting a string for names written in lowercase
+    private greetingMultiplePeopleNormally(lowerCaseNames: string[]): string{
+        if (lowerCaseNames.length === 0){
+            return ""
         }else{
-            if(name.length === 1){
-                if(name[0] === name[0].toUpperCase())
-                    return "GREETINGS, "+ name[0].toUpperCase() + "!";
-                else
-                    return "Greetings, " + name + "!";
+
+            if(lowerCaseNames.length === 1){
+                return "Greetings, " + lowerCaseNames[0];
             }else{
-                let nameString: string = "Greetings, ";
-                for(let i = 0; i < name.length-1; i++){
-                    nameString += name[i] + ", ";
+                let partOfGreeting:string;
+                partOfGreeting = "";
+                
+                partOfGreeting = "Greetings, ";
+
+                for(let i = 0; i < lowerCaseNames.length-1; i++){
+                    partOfGreeting += lowerCaseNames[i] + ", ";
                 }
-                nameString += "and " + name[name.length-1] + "!";
-    
-                return nameString;
+
+                partOfGreeting += "and " + lowerCaseNames[lowerCaseNames.length-1];
+
+                return partOfGreeting;
             }
-            
         }
     }
 
+    //constracting a string for names written in uppercase
+    private greetingMultiplePeopleLoudly(upperCaseNames: string[]): string{
+        if (upperCaseNames.length === 0){
+            return ""
+        }else{
+            if(upperCaseNames.length === 1){
 
+                return "GREETINGS, " + upperCaseNames[0];
+            }else{
+
+                let partOfGreeting:string;
+
+                partOfGreeting = "GREETINGS, ";
+
+                for(let i = 0; i < upperCaseNames.length-1; i++){
+                    partOfGreeting += upperCaseNames[i] + ", ";
+                }
+        
+                partOfGreeting += "AND " + upperCaseNames[upperCaseNames.length-1];
+        
+                return partOfGreeting;
+            }
+        }
+           
+    }
+
+
+    //constracting the final greeting in a string
+    public greeting(...names :string[]): String{
+        if(names.length === 0){
+            return "Greetings, my friend!"
+        }else{
+            let firstPartOfFinalGreeting: string;
+            let lastPartOfFinalGreeting: string;
+
+            firstPartOfFinalGreeting = "";
+            lastPartOfFinalGreeting = "";
+
+            let numberOfLowNames: number;
+            let numberOfUpNames: number;
+
+            let lowerCaseNamesArray= new Array();
+            let upperCaseNamesArray= new Array();
+
+            for(let i = 0; i < names.length; i++){
+                if(this.isItUpperCase(names[i])){
+                    upperCaseNamesArray.push(names[i]);
+                }else{
+                    lowerCaseNamesArray.push(names[i]);
+                }
+            }
+
+            firstPartOfFinalGreeting = this.greetingMultiplePeopleNormally(lowerCaseNamesArray);       
+            lastPartOfFinalGreeting = this.greetingMultiplePeopleLoudly(upperCaseNamesArray);
+
+            if(firstPartOfFinalGreeting !== "" ){
+                if(lastPartOfFinalGreeting !== "")
+                    return firstPartOfFinalGreeting + ". AND " + lastPartOfFinalGreeting + "!";
+                else
+                    return firstPartOfFinalGreeting + "!";
+            }else{
+                return lastPartOfFinalGreeting + "!";
+            }
+        }
+    }
 }
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -31,6 +31,7 @@ describe('greeting', function () {
     expect(greetingString).toBe("Greetings, John, Bob, Kate, Alex, and Hunter!");
   });
 
+
   it('should convert to uppercase when shouting', function () {
     let testGreeter = new Greeter;
     let greetingString: String;
@@ -38,7 +39,28 @@ describe('greeting', function () {
     expect(greetingString).toBe("GREETINGS, JOHN!");
   });
 
-  
+  it('should greet multiple people shouting', function () {
+    let testGreeter = new Greeter;
+    let greetingString: String;
+    greetingString =  testGreeter.greeting("JOHN", "HANNA", "BOB", "ANNA", "ALEX", "KATE");
+    expect(greetingString).toBe("GREETINGS, JOHN, HANNA, BOB, ANNA, ALEX, AND KATE!");
+  });
+
+  it('should sort and convert to uppercase when greeting multiple people 3 3', function () {
+    let testGreeter = new Greeter;
+    let greetingString: String;
+    greetingString =  testGreeter.greeting("JOHN", "Hanna", "Bob", "Anna", "ALEX", "KATE");
+    expect(greetingString).toBe("Greetings, Hanna, Bob, and Anna. AND GREETINGS, JOHN, ALEX, AND KATE!");
+  });
+
+  it('should sort and convert to uppercase when greeting multiple people 5 1 ', function () {
+    let testGreeter = new Greeter;
+    let greetingString: String;
+    greetingString =  testGreeter.greeting("John", "Hanna", "Bob", "Anna", "Alex", "KATE");
+    expect(greetingString).toBe("Greetings, John, Hanna, Bob, Anna, and Alex. AND GREETINGS, KATE!");
+  });
+
+
 
 
 });
