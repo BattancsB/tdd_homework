d4eb88ccb4504b1d7e7bb4d847727818c7344a39
Szabó-Battancs Benedek <h880561@stud.u-szeged.hu>
2021-10-13 01:01:20 +0200

    refactor

--- src/greeter.ts
+++ src/greeter.ts
@@ -11,57 +11,29 @@ export class Greeter{
         return input === input.toUpperCase();
     }
 
-    //constracting a string for names written in lowercase
-    private greetingMultiplePeopleNormally(lowerCaseNames: string[]): string{
-        if (lowerCaseNames.length === 0){
+    //constracting a string for names 
+    private greetingMultiplePeople(namesArray: string[]): string{
+        if (namesArray.length === 0){
             return ""
         }else{
 
-            if(lowerCaseNames.length === 1){
-                return "Greetings, " + lowerCaseNames[0];
+            if(namesArray.length === 1){
+                return "Greetings, " + namesArray[0];
             }else{
                 let partOfGreeting:string;
-                partOfGreeting = "";
                 
                 partOfGreeting = "Greetings, ";
 
-                for(let i = 0; i < lowerCaseNames.length-1; i++){
-                    partOfGreeting += lowerCaseNames[i] + ", ";
+                for(let i = 0; i < namesArray.length-1; i++){
+                    partOfGreeting += namesArray[i] + ", ";
                 }
 
-                partOfGreeting += "and " + lowerCaseNames[lowerCaseNames.length-1];
+                partOfGreeting += "and " + namesArray[namesArray.length-1];
 
                 return partOfGreeting;
             }
         }
     }
-
-    //constracting a string for names written in uppercase
-    private greetingMultiplePeopleLoudly(upperCaseNames: string[]): string{
-        if (upperCaseNames.length === 0){
-            return ""
-        }else{
-            if(upperCaseNames.length === 1){
-
-                return "GREETINGS, " + upperCaseNames[0];
-            }else{
-
-                let partOfGreeting:string;
-
-                partOfGreeting = "GREETINGS, ";
-
-                for(let i = 0; i < upperCaseNames.length-1; i++){
-                    partOfGreeting += upperCaseNames[i] + ", ";
-                }
-        
-                partOfGreeting += "AND " + upperCaseNames[upperCaseNames.length-1];
-        
-                return partOfGreeting;
-            }
-        }
-           
-    }
-
 
     //constracting the final greeting in a string
     public greeting(...names :string[]): String{
@@ -71,25 +43,22 @@ export class Greeter{
             let firstPartOfFinalGreeting: string;
             let lastPartOfFinalGreeting: string;
 
-            firstPartOfFinalGreeting = "";
-            lastPartOfFinalGreeting = "";
-
-            let numberOfLowNames: number;
-            let numberOfUpNames: number;
-
             let lowerCaseNamesArray= new Array();
             let upperCaseNamesArray= new Array();
 
             for(let i = 0; i < names.length; i++){
-                if(this.isItUpperCase(names[i])){
-                    upperCaseNamesArray.push(names[i]);
-                }else{
-                    lowerCaseNamesArray.push(names[i]);
+                let splittedNames =names[i].split(", ", 1000);
+                for(let name of splittedNames){
+                    if(this.isItUpperCase(name)){
+                        upperCaseNamesArray.push(name);
+                    }else{
+                        lowerCaseNamesArray.push(name);
+                    }
                 }
             }
 
-            firstPartOfFinalGreeting = this.greetingMultiplePeopleNormally(lowerCaseNamesArray);       
-            lastPartOfFinalGreeting = this.greetingMultiplePeopleLoudly(upperCaseNamesArray);
+            firstPartOfFinalGreeting = this.greetingMultiplePeople(lowerCaseNamesArray);       
+            lastPartOfFinalGreeting = this.greetingMultiplePeople(upperCaseNamesArray).toUpperCase();
 
             if(firstPartOfFinalGreeting !== "" ){
                 if(lastPartOfFinalGreeting !== "")
