6ff4860794769c6fb4fae2412de6ac21c88d6d75
Szabó-Battancs Benedek <h880561@stud.u-szeged.hu>
2021-10-20 23:09:37 +0200

    fixed tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -2,7 +2,7 @@ import {Greeter} from "./greeter";
 
 describe('greeting', function () {
 
-  it('greeting my friend with unknown name', function () {
+  it('greeting my friend with an unknown name', function () {
     let testGreeter = new Greeter;
     let greetingString: String;
     greetingString =  testGreeter.greeting();
@@ -24,12 +24,6 @@ describe('greeting', function () {
     expect(greetingString).toBe("Greetings, John, Bob, and Kate!");
   });
 
-  it('greeting 5 people at once', function () {
-    let testGreeter = new Greeter;
-    let greetingString: String;
-    greetingString =  testGreeter.greeting("John", "Bob", "Kate", "Alex", "Hunter");
-    expect(greetingString).toBe("Greetings, John, Bob, Kate, Alex, and Hunter!");
-  });
 
 
   it('should convert to uppercase when shouting', function () {
@@ -46,28 +40,29 @@ describe('greeting', function () {
     expect(greetingString).toBe("GREETINGS, JOHN, HANNA, BOB, ANNA, ALEX, AND KATE!");
   });
 
-  it('should sort and convert to uppercase when greeting multiple people 3 3', function () {
-    let testGreeter = new Greeter;
-    let greetingString: String;
-    greetingString =  testGreeter.greeting("JOHN", "Hanna", "Bob", "Anna", "ALEX", "KATE");
-    expect(greetingString).toBe("Greetings, Hanna, Bob, and Anna. AND GREETINGS, JOHN, ALEX, AND KATE!");
-  });
-
-  it('should sort and convert to uppercase when greeting multiple people 5 1 ', function () {
+  it('should greet people differently when there are some names written in lowercase and one written in uppercase', function () {
     let testGreeter = new Greeter;
     let greetingString: String;
     greetingString =  testGreeter.greeting("John", "Hanna", "Bob", "Anna", "Alex", "KATE");
     expect(greetingString).toBe("Greetings, John, Hanna, Bob, Anna, and Alex. AND GREETINGS, KATE!");
   });
 
-  it('should split input names with commas', function () {
+  it('should group and handle names when some of them are written in lowercase and some written in uppercase', function () {
+    let testGreeter = new Greeter;
+    let greetingString: String;
+    greetingString =  testGreeter.greeting("JOHN", "Hanna", "Bob", "Anna", "ALEX", "KATE");
+    expect(greetingString).toBe("Greetings, Hanna, Bob, and Anna. AND GREETINGS, JOHN, ALEX, AND KATE!");
+  });
+
+
+  it('should split input names with commas when all the names written in lowercase', function () {
     let testGreeter = new Greeter;
     let greetingString: String;
     greetingString =  testGreeter.greeting("John", "Hanna, Bob", "Anna, Alex, Kate");
     expect(greetingString).toBe("Greetings, John, Hanna, Bob, Anna, Alex, and Kate!");
   });
 
-  it('should split input names with commas', function () {
+  it('should split input names with commas when there are names written in upercase and written in lowercase together', function () {
     let testGreeter = new Greeter;
     let greetingString: String;
     greetingString =  testGreeter.greeting("John", "Hanna, BOB, Anna, Alex", "KATE");
